9f27eb8868f1b91992efbc26730bf61eb3ffa91e
unknown <douglas.luzinda.ot@gmail.com>
2023-09-16 18:47:04 +0300

    add factorial method

--- Solvers.rb
+++ Solvers.rb
@@ -2,5 +2,13 @@
 
 # Solver class provides methods for solving mathematical problems
 class Solver
+  # Calculates the factorial of a non-negative integer
+  def self.factorial(number)
+    raise ArgumentError, "Input must be a non-negative integer" if number.negative?
+    return 1 if number.zero?
+
+    (1..number).reduce(:*)
+  end
+
 
 end
